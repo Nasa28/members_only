22634f64d2bec8ae231a5ca3ae12d6843b57b42a
Nasa28 <kaluchinasaagu@gmail.com>
2021-03-19 16:54:20 +0100

    add create action to posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,27 @@
 class PostsController < ApplicationController
   before_action  only: [:new, :create]
-  
+
   def new
     @post = Post.new
   end
   
+  def index
+    @posts = Post.all
+  end
+ 
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path, notice:  "Post was successfully created."
+    else
+      render 'new'
+  end
+
+  private
+
+  def post_params 
+    params.require(:post).permit(:title, :body)
+
+  end
 end
